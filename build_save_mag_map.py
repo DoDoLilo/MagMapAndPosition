A backup before delete the option of using orientation sensor in the system. After contrast experiments, we found the game rotation sensor can fix the unstable mag vector decomposition problem to mv\mh. So I made up my mind to delete the orientation sensor related codes, to clean the codes. But the orientation sensor may be used to (just) more contrast experiments in the future (maybe for paper), so the backup was made.

build_save_mag_map.py
@@ -21,11 +21,11 @@ DELETE_LEVEL = -3
 USE_ORIENTATION = False
 
 file_paths_build_map = [
-    # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-519-1-4.247409484433081 Pixel 3a_sync.csv"
+    # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-519-1-4.247409484433081 Pixel 3a_sync.csv",
     # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-519-3-182.20603680993108 Pixel 3a_sync.csv"
     # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-523-4-16.85575427716903 Pixel 3a_sync.csv",
     # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-523-5-174.51484401105918 Pixel 3a_sync.csv"
-    # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-524-6-180.96802404173124 Pixel 3a_sync.csv",
+    "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-524-6-180.96802404173124 Pixel 3a_sync.csv",
     "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-524-7-184.01945203456944 Pixel 3a_sync.csv"
     # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-524-8-182.21094088575512 Pixel 3a_sync.csv"
     # "data/data_test/data_to_building_map/one_floor_hall_hallway/IMU-524-9-8.673526599631316 Pixel 3a_sync.csv"
@@ -44,7 +44,7 @@ mag_map = MMT.build_map_by_files(
 
 # mag_map保存到 data/data_test/mag_map
 # mag_map[i][j][mv][mh]
-save_path = 'data/data_test/mag_map/one_floor_hall_hallway/map_F7_B30_full'
+save_path = 'data/data_test/mag_map/one_floor_hall_hallway/map_F6_7_B30_full'
 np.savetxt(save_path + '/mv_qiu_2d.csv', mag_map[:, :, 0], delimiter=',')
 np.savetxt(save_path + '/mh_qiu_2d.csv', mag_map[:, :, 1], delimiter=',')
 print("Save files to:", save_path)

mag_mapping_tools.py
@@ -6,6 +6,7 @@ from dtaidistance import dtw
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.spatial.transform import Rotation
+import matplotlib.lines as mlines
 
 
 # 读csv文件所有列，返回指定列：输入：文件路径，返回：指定列数组numpy.ndarray
@@ -95,15 +96,15 @@ def get_mag_vh_arr(angles_arr, mag_arr, use_orientation):
     return np.array(list_mv_mh)
 
 
-# q为四元数
+# q为四元数（game rotation vector)
 # 输入为序列，直接并行处理，不再是一个一个处理
 # 返回垂直磁强、水平磁强和总磁强
 def get_2d_mag_qiu(q, mag):
     ori_R = Rotation.from_quat(q)
     glob_mag = np.einsum("tip,tp->ti", ori_R.as_matrix(), mag)
-    mag_v = np.abs(glob_mag[:, 2:3])
-    mag_h = np.linalg.norm(glob_mag[:, 0:2], axis=1, keepdims=True)
-    mag_total = np.linalg.norm(glob_mag, axis=1, keepdims=True)
+    mag_v = np.abs(glob_mag[:, 2:3])  # 垂直分量 = z轴结果
+    mag_h = np.linalg.norm(glob_mag[:, 0:2], axis=1, keepdims=True)  # 水平分量 = x, y的合
+    mag_total = np.linalg.norm(glob_mag, axis=1, keepdims=True)  # 总量 = x,y,z的合
     return np.concatenate([mag_v, mag_h, mag_total], axis=1)
 
 
@@ -734,6 +735,32 @@ def get_PDR_xy_mag_ori(pdr_xy, raw_mag, raw_ori, pdr_frequency=20, sampling_freq
                                raw_mag[raw_i][0], raw_mag[raw_i][1], raw_mag[raw_i][2],
                                raw_ori[raw_i][0], raw_ori[raw_i][1], raw_ori[raw_i][2]])
     return np.array(PDR_xy_mag_ori)
+
+
+def get_PDR_xy_align_mag_quat(pdr_xy, raw_mag, raw_quat, pdr_frequency=20, sampling_frequency=200):
+    if sampling_frequency < pdr_frequency:
+        print("Wrong frequency in get_PDR_xy_mag_ori: sampling_frequency < PDR_frequency")
+        return None
+    if sampling_frequency % pdr_frequency != 0:
+        print("Wrong frequency in get_PDR_xy_mag_ori: sampling_frequency % PDR_frequency != 0")
+        return None
+
+    window_size = int(sampling_frequency / pdr_frequency)
+    if window_size < 1:
+        print("get_PDR_xy_align_mag_quat(): Wrong PDR\simpling frequency!")
+        return None
+
+    PDR_xy_mag_quat = []
+    for i in range(0, len(pdr_xy)):
+        raw_i = i * window_size
+        # 越界则提前跳出循环，按理说正确的[pdr_frequency, simpling_frequency]不会有这种情况
+        if raw_i >= len(raw_mag):
+            print("get_PDR_xy_mag_ori(): Drop out in break!")
+            break
+        PDR_xy_mag_quat.append([pdr_xy[i][0], pdr_xy[i][1],
+                                raw_mag[raw_i][0], raw_mag[raw_i][1], raw_mag[raw_i][2],
+                                raw_quat[raw_i][0], raw_quat[raw_i][1], raw_quat[raw_i][2], raw_quat[raw_i][3]])
+    return np.array(PDR_xy_mag_quat)
 
 
 # 输入的data_xy变为不同频率的PDR_xy的缓冲池函数，区别在于内置了对原始数据与PDRxy数据之间的对齐、平均的操作

my_test/find_start_transfer.py
@@ -15,10 +15,10 @@ import mag_mapping_tools as MMT
 import numpy as np
 import math
 
-MOVE_X = 7
-MOVE_Y = 8
-MAP_SIZE_X = 8
-MAP_SIZE_Y = 13
+MOVE_X = 25.
+MOVE_Y = 12.
+MAP_SIZE_X = 58.
+MAP_SIZE_Y = 16.
 BLOCK_SIZE = 0.25
 # 低通滤波的程度，值越大滤波越强。整型，无单位。
 EMD_FILTER_LEVEL = 3
@@ -29,21 +29,28 @@ DELETE_LEVEL = 1
 BUFFER_DIS = 5
 DOWN_SIP_DIS = BLOCK_SIZE
 
+# PATH_PDR_RAW[0]是pdr的xy.npy文件,[1]是增加了真值的手机IMU数据文件.csv
+ORIGINAL_START_TRANSFER = [38.9, 11.8, math.radians(-97.)]
+PATH_PDR_RAW = [
+    "../data/data_test/data_to_position_pdr/one_floor_hall_hallway/pdr/IMU-607-4-187.68290595817584 Pixel 3a.csv.npy",
+    "../data/data_test/data_to_position_pdr/one_floor_hall_hallway/gt/IMU-607-4-187.68290595817584 Pixel 3a_sync.csv"]
 
-path_pdr_raw = ["../data/data_test/pdr/IMU-10-6-178.00767980919863 Pixel 6.csv.npy",
-                "../data/data_test/data_to_building_map/IMU-10-6-178.00767980919863 Pixel 6_sync.csv"]
+pdr_xy = np.load(PATH_PDR_RAW[0])[:, 0:2]
 
-pdr_xy = np.load(path_pdr_raw[0])[:, 0:2]
-data_all = MMT.get_data_from_csv(path_pdr_raw[1])
+data_all = MMT.get_data_from_csv(PATH_PDR_RAW[1])
+raw_quat = data_all[:, 7:11]
 raw_mag = data_all[:, 21:24]
 raw_ori = data_all[:, 18:21]
 raw_xy = data_all[:, np.shape(data_all)[1] - 5:np.shape(data_all)[1] - 3]
-PDR_xy_mag_ori = MMT.get_PDR_xy_mag_ori(pdr_xy, raw_mag, raw_ori)
-pdr_data_mag = PDR_xy_mag_ori[:, 2:5]
-pdr_data_ori = PDR_xy_mag_ori[:, 5:8]
-pdr_data_xy = pdr_xy
+# PDR_xy_mag_ori = MMT.get_PDR_xy_mag_ori(pdr_xy, raw_mag, raw_ori)
+# pdr_data_mag = PDR_xy_mag_ori[:, 2:5]
+# pdr_data_ori = PDR_xy_mag_ori[:, 5:8]
+PDR_xy_mag_quat = MMT.get_PDR_xy_align_mag_quat(pdr_xy, raw_mag, raw_quat)
+mag_pdr = PDR_xy_mag_quat[:, 2:5]
+quat_pdr = PDR_xy_mag_quat[:, 5:9]
+xy_pdr = pdr_xy
 # 并不在此时修改pdr_xy坐标，match_seq_list=多条匹配序列[?][?][x,y, mv, mh]
-match_seq_list = MMT.samples_buffer(BUFFER_DIS, DOWN_SIP_DIS, pdr_data_ori, pdr_data_mag, pdr_data_xy,
+match_seq_list = MMT.samples_buffer(BUFFER_DIS, DOWN_SIP_DIS, quat_pdr, mag_pdr, xy_pdr,
                                     do_filter=True, lowpass_filter_level=EMD_FILTER_LEVEL)
 map_xy_list = []
 for i in range(0, len(match_seq_list)):
@@ -57,10 +64,13 @@ for map_xy in map_xy_list:
 final_xy = np.array(final_xy)
 # MMT.paint_xy(pdr_xy)
 # MMT.paint_xy(final_xy)
-new_pdr_xy = np.array(MMT.transfer_axis_list(final_xy, [6.8, 2.4, math.radians(-100.)]))
-MMT.paint_xy(new_pdr_xy, xy_range=[0, MAP_SIZE_X, 0, MAP_SIZE_Y])
 MMT.change_axis(raw_xy, MOVE_X, MOVE_Y)
-MMT.paint_xy(raw_xy, xy_range=[0, MAP_SIZE_X, 0, MAP_SIZE_Y])
+MMT.change_axis(final_xy, MOVE_X, MOVE_Y)
+new_pdr_xy = np.array(MMT.transfer_axis_list(final_xy, ORIGINAL_START_TRANSFER))
+# MMT.paint_xy(new_pdr_xy, xy_range=[0, MAP_SIZE_X, 0, MAP_SIZE_Y])
+
+# MMT.paint_xy(raw_xy, xy_range=[0, MAP_SIZE_X, 0, MAP_SIZE_Y])
+MMT.paint_xy(np.vstack((raw_xy, new_pdr_xy)), xy_range=[0, MAP_SIZE_X, 0, MAP_SIZE_Y])
 print("First Points, PDR:\n{0}, \niLocator:\n{1}".format(new_pdr_xy[0:5], raw_xy[0:5]))
 
 

my_test/test_mag_match_PDR_search_again_when_failed.py
@@ -2,14 +2,15 @@ import math
 import mag_mapping_tools as MMT
 import numpy as np
 import my_test.test_tools as TEST
-from scipy.spatial.transform import Rotation
+
+# from scipy.spatial.transform import Rotation
 
 # iLocator真值坐标平移参数
-MOVE_X = 7
-MOVE_Y = 8
+MOVE_X = 25.
+MOVE_Y = 12.
 # 地图坐标系大小 0-MAP_SIZE_X ，0-MAP_SIZE_Y
-MAP_SIZE_X = 8.
-MAP_SIZE_Y = 13.
+MAP_SIZE_X = 58.
+MAP_SIZE_Y = 16.
 # 地图地磁块大小
 BLOCK_SIZE = 0.25
 # 低通滤波的程度，值越大滤波越强。整型，无单位。
@@ -22,31 +23,40 @@ DOWN_SIP_DIS = BLOCK_SIZE
 # 高斯牛顿最大迭代次数
 MAX_ITERATION = 90
 # 目标损失
-TARGET_LOSS = BUFFER_DIS / BLOCK_SIZE * 15
+TARGET_LOSS = BUFFER_DIS / BLOCK_SIZE * 10
 print("TARGET_LOSS:", TARGET_LOSS, '\n')
 # 迭代步长，牛顿高斯迭代是局部最优，步长要小
-STEP = 1 / 70
+STEP = 1 / 50
 # 原始数据采样频率 , PDR坐标输出频率
 SAMPLE_FREQUENCY = 200
 PDR_XY_FREQUENCY = 20
 # 是否使用orientation传感器获取手机姿态角
 USE_ORIENTATION = False
+# 枚举transfers的产生参数，[0] = [△x, △y(米), △angle(弧度)] , [1] = [正负个数]
+TRANSFERS_PRODUCE_CONFIG = [[0.25, 0.25, math.radians(1.5)], [10, 10, 10]]
 # 首次迭代固定区域遍历数组，默认起点在某一固定区域，transfer=[△x,△y,△angle]，
 # Transfer[△x, △y(米), △angle(弧度)]：先绕原坐标原点逆时针旋转，然后再平移
-ORIGINAL_START_TRANSFER = [6.7, 1.7, math.radians(-98.)]
-START_CONFIG = [[0.25, 0.25, math.radians(1.)], [3, 3, 3]]
-START_TRANSFERS = MMT.produce_transfer_candidates_ascending(ORIGINAL_START_TRANSFER, START_CONFIG)
+ORIGINAL_START_TRANSFER = [0, 0, math.radians(0.)]
 PATH_PDR_RAW = [
-    "../data/data_test/data_to_position_pdr/data_server_room/pdr/IMU-10-2-183.5307793202117 Pixel 6.csv.npy",
-    "../data/data_test/data_to_building_map/server_room/IMU-10-2-183.5307793202117 Pixel 6_sync.csv"]
+    "../data/data_test/data_to_position_pdr/one_floor_hall_hallway/aligned_pdr/IMU-607-4-187.68290595817584 Pixel 3a_sync.csv.npy",
+    "../data/data_test/data_to_position_pdr/one_floor_hall_hallway/gt/IMU-607-4-187.68290595817584 Pixel 3a_sync.csv"]
+
+print("ORIGINAL_START_TRANSFER:", ORIGINAL_START_TRANSFER)
+# 地磁指纹库文件，[0]为mv.csv，[1]为mh.csv
+PATH_MAG_MAP = [
+    "../data/data_test/mag_map/one_floor_hall_hallway/map_F1_3_B25_full/mv_qiu_2d.csv",
+    "../data/data_test/mag_map/one_floor_hall_hallway/map_F1_3_B25_full/mh_qiu_2d.csv"
+]
+# ----------------------------
+paint_map_size = [0, MAP_SIZE_X * 1.0, 0, MAP_SIZE_Y * 1.0]
 
 
 def main():
     # 全流程
     # 1.png、建库
     # 读取提前建库的文件，并合并生成原地磁指纹地图mag_map
-    mag_map_mv = np.array(np.loadtxt('../data/data_test/mag_map/server_room/mag_map_mv.csv', delimiter=','))
-    mag_map_mh = np.array(np.loadtxt('../data/data_test/mag_map/server_room/mag_map_mh.csv', delimiter=','))
+    mag_map_mv = np.array(np.loadtxt(PATH_MAG_MAP[0], delimiter=','))
+    mag_map_mh = np.array(np.loadtxt(PATH_MAG_MAP[1], delimiter=','))
     mag_map = []
     for i in range(0, len(mag_map_mv)):
         temp = []
@@ -54,7 +64,7 @@ def main():
             temp.append([mag_map_mv[i][j], mag_map_mh[i][j]])
         mag_map.append(temp)
     mag_map = np.array(mag_map)
-    # MMT.paint_heat_map(mag_map, show_mv=False)
+    # MMT.paint_heat_map(mag_map)
 
     # 2、缓冲池给匹配段（内置稀疏采样），此阶段的data与上阶段无关
     pdr_xy = np.load(PATH_PDR_RAW[0])[:, 0:2]
@@ -62,6 +72,8 @@ def main():
     iLocator_xy = data_all[:, np.shape(data_all)[1] - 5:np.shape(data_all)[1] - 3]
     # 将iLocator_xy的坐标转换到MagMap中，作为Ground Truth
     MMT.change_axis(iLocator_xy, MOVE_X, MOVE_Y)
+    MMT.change_axis(pdr_xy, MOVE_X, MOVE_Y)
+    MMT.paint_xy(np.vstack((pdr_xy, iLocator_xy)), None, paint_map_size)
     data_mag = data_all[:, 21:24]
     # data_ori = data_all[:, 18:21]
     data_quat = data_all[:, 7:11]
@@ -82,10 +94,10 @@ def main():
     # 情况AB表示匹配失败
     #   3.1第一次匹配进行起点周围区域遍历，遍历后选出残差最小的
     first_match = np.array(match_seq_list[0])
-    print("*Match Seq 0 : first deep search seq.")
-    transfer = MMT.produce_transfer_candidates_search_again(ORIGINAL_START_TRANSFER, START_CONFIG,
+    print("*Match Seq 0 : first search seq, deep ? or not?")
+    transfer = MMT.produce_transfer_candidates_search_again(ORIGINAL_START_TRANSFER, TRANSFERS_PRODUCE_CONFIG,
                                                             first_match, mag_map, BLOCK_SIZE, STEP, MAX_ITERATION,
-                                                            TARGET_LOSS, break_advanced=False)
+                                                            TARGET_LOSS, break_advanced=True)
     out_of_map, loss, map_xy, not_used_transfer = MMT.cal_new_transfer_and_last_loss_xy(
         transfer, first_match, mag_map, BLOCK_SIZE, STEP
     )
@@ -121,7 +133,7 @@ def main():
             if not out_of_map:
                 loss_list.append(loss)
                 if loss <= TARGET_LOSS:
-                    print("\tSucceed in iteration", iter_num)
+                    print("\tSucceed in iteration", iter_num, ", loss list:", loss_list)
                     # 成功了怎么办：提前结束迭代，先不添加该结果，等待后续特征判断
                     break
 
@@ -131,7 +143,7 @@ def main():
                 # 在迭代前备份的start_transfer基础上进行范围搜索，范围从近到远，匹配成功则提前结束。
                 print("\tFailed in iteration", iter_num, ", loss list:", loss_list,
                       "\n\tSearch more in the start_transfer ... ...")
-                transfer = MMT.produce_transfer_candidates_search_again(start_transfer, START_CONFIG,
+                transfer = MMT.produce_transfer_candidates_search_again(start_transfer, TRANSFERS_PRODUCE_CONFIG,
                                                                         match_seq, mag_map,
                                                                         BLOCK_SIZE, STEP, MAX_ITERATION,
                                                                         TARGET_LOSS, break_advanced=True)
@@ -150,8 +162,8 @@ def main():
                 mag_map_mvh.append(map_mvh)
                 mag_map_grads.append(grad)
             mag_map_mvh = np.array(mag_map_mvh)
-            MMT.paint_signal(mag_map_mvh[:, 0], "Map mv Seq {0}".format(i))
-            MMT.paint_signal(mag_map_mvh[:, 1], "Map mh Seq {0}".format(i))
+            # MMT.paint_signal(mag_map_mvh[:, 0], "Map mv Seq {0}".format(i))
+            # MMT.paint_signal(mag_map_mvh[:, 1], "Map mh Seq {0}".format(i))
             std_deviation_mv, std_deviation_mh, std_deviation_all = MMT.cal_std_deviation_mag_vh(mag_map_mvh)  # 标准差
             unsameness_mv, unsameness_mh, unsameness_all = MMT.cal_unsameness_mag_vh(mag_map_mvh)  # 相邻不相关程度
             grad_level_mv, grad_level_mh, grad_level_all = MMT.cal_grads_level_mag_vh(mag_map_grads)  # 整体梯度水平
@@ -168,8 +180,8 @@ def main():
 
         # 计算实测序列中的地磁序列的特征程度，并打印输出结果
         mag_vh_arr = match_seq[:, 2:4]
-        MMT.paint_signal(mag_vh_arr[:, 0], "Real mv Seq {0}".format(i))
-        MMT.paint_signal(mag_vh_arr[:, 1], "Real mh Seq {0}".format(i))
+        # MMT.paint_signal(mag_vh_arr[:, 0], "Real mv Seq {0}".format(i))
+        # MMT.paint_signal(mag_vh_arr[:, 1], "Real mh Seq {0}".format(i))
         std_deviation_mv, std_deviation_mh, std_deviation_all = MMT.cal_std_deviation_mag_vh(mag_vh_arr)  # 标准差
         unsameness_mv, unsameness_mh, unsameness_all = MMT.cal_unsameness_mag_vh(mag_vh_arr)  # 相邻不相关程度
         print("\tFeatures of real time mag: "
@@ -240,11 +252,15 @@ def main():
     mean_distance = np.mean(distance_of_MagPDR_iLocator_points[:, 0])
     print("\tMean Distance between MagPDR and GT: ", mean_distance)
     # 5.3 对Ground Truth(iLocator)、PDR、MagPDR进行绘图
-    MMT.paint_xy(iLocator_xy, "The Ground Truth by iLocator", [0, MAP_SIZE_X * 1.0, 0, MAP_SIZE_Y * 1.0])
-    MMT.paint_xy(pdr_xy, "The PDR", [0, MAP_SIZE_X * 1.0, 0, MAP_SIZE_Y * 1.0])
+    MMT.paint_xy(iLocator_xy, "The Ground Truth by iLocator", paint_map_size)
+    MMT.paint_xy(pdr_xy, "The PDR", paint_map_size)
     MMT.paint_xy(final_xy, "The MagPDR: BlockSize={0}, BufferDis={1}, MaxIteration={2}, Step={3:.8f}, TargetLoss={4}"
                  .format(BLOCK_SIZE, BUFFER_DIS, MAX_ITERATION, STEP, TARGET_LOSS),
-                 [0, MAP_SIZE_X * 1.0, 0, MAP_SIZE_Y * 1.0])
+                 paint_map_size)
+    contrast_xy = np.vstack((iLocator_xy, np.vstack((pdr_xy, final_xy))))
+    MMT.paint_xy(contrast_xy, "Three contrast xy", paint_map_size)
+    # paint_single_heat_map(mag_map, [gt, pos, cali_positions], ['line', 'line', 'line'], ['真实轨迹','PDR轨迹','EKF融合轨迹'],['black', 'purple', 'red'])
+
     return
 
 